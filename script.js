2503ccef73653495d8abc1c787c5c9853da85c92
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-23 17:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your code here.
+const container = document.querySelector('.container');
+let isMouseDown = false;
+let startX;
+let scrollLeft;
+
+container.addEventListener('mousedown', (e) => {
+  isMouseDown = true;
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('mouseleave', () => {
+  isMouseDown = false;
+});
+
+container.addEventListener('mouseup', () => {
+  isMouseDown = false;
+});
+
+container.addEventListener('mousemove', (e) => {
+  if (!isMouseDown) return;
+  e.preventDefault();
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * 3;
+  container.scrollLeft = scrollLeft - walk;
+});
